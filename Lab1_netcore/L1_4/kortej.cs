dffb3a442c45c4671eabb792bc23312595321847
howtojumpq <94301667+howtojumpq@users.noreply.github.com>
2022-09-18 16:35:27 +0300

    second commit
    added tasks 4 and 5

---
+++ Lab1_netcore/L1_4/kortej.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задаём кортеж
+            (int, string, char, string, ulong) t1 = (1, "hello", 'a', "world", 12);
+            Console.WriteLine(" ");
+            //вывод всех элементов кортежа:
+            Console.WriteLine(t1);
+            //выборочный вывод элементов:
+            Console.WriteLine("Первый и третий элементы кортежа: " + t1.Item1 + " " + "," + " " + t1.Item3);
+            //деконструкция кортежей:
+            int dekonstint;
+            string dekonststring1;
+            char dekonstchar;
+            string dekonststring2;
+            ulong dekonstulong;
+            (dekonstint, dekonststring1, dekonstchar, dekonststring2, dekonstulong) = t1;
+            Console.WriteLine(" ");
+            Console.WriteLine(" Переменные, содержащие элементы кортежа: ");
+            Console.WriteLine(dekonstint + " " + dekonststring1 + " " + dekonstchar + " " + dekonststring2 + " " + dekonstulong);
+
+            //сравнение кортежей:
+            var tuple1 = (23, 36);
+            var tuple2 = (17, 31);
+            Console.WriteLine(" ");
+            //при сравнении кортежей все элементы кортежа должны иметь возможности сравнения
+            //кол-во элементов должно быть одинаковым
+            Console.WriteLine(tuple1 == tuple2);//False
+            Console.WriteLine(tuple1 != tuple2);//True
+
+        }
+    }
+}
---
+++ Lab1_netcore/L1_5/local_function.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace L1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int,int,char) Func1(int[] massiv, string stroka,int massivlength)
+            {
+                (int, int, char) kortej;
+                int massivmaxnumber=massiv[0];
+                int massivminnumber=massiv[0];
+                int sumofnumbers = 0;
+
+                //рассчитываем min и max элементы
+                for(int i = 1; i < 3; i++)
+                {
+                    if (massivmaxnumber < massiv[i])
+                    {
+                        massivmaxnumber = massiv[i];
+                    }
+
+                    if (massivminnumber > massiv[i])
+                    {
+                        massivminnumber = massiv[i];
+                    }
+
+                }
+                //рассчитываем сумму всех элементов
+                for(int i = 0; i < 3; i++)
+                {
+                    sumofnumbers = sumofnumbers + massiv[i];
+                }
+
+                kortej = (massivmaxnumber, massivminnumber, stroka[0]);
+                return kortej;
+
+            };
+
+            const int massivlength = 3;
+
+            int[] massivintov = new int[massivlength] { 1, 2, 3 };
+            string stroka = "zxc";
+
+            //результат работы функции
+            Console.WriteLine(Func1(massivintov, stroka, massivlength));
+
+
+        }
+    }
+}
